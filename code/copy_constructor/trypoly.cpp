9fb12cd16de99c403f2aae182dcdc2cc91038b2c
ykrsama <582697760.com>
2021-11-28 17:27:16 +0800

    update

--- code/copy_constructor/trypoly.cpp
+++ code/copy_constructor/trypoly.cpp
@@ -5,8 +5,8 @@ int main() {
     Hexagon hexa1(1);
     Hexagon hexa2 = hexa1;
     std::cout << "Hexagon1,2 Perimter : " << hexa1.computePerimeter() << '\n';
-    std::cout << "Modify radius of Hexagon1 from 1 to 2\n";
-    hexa1.setRadius(2);
+    std::cout << "Set Hexagon1 radius to 3\n";
+    hexa1.setRadius(3);
     std::cout << "Hexagon1 Perimter : " << hexa1.computePerimeter() << '\n';
     std::cout << "Hexagon2 Perimter : " << hexa2.computePerimeter() << '\n';
 }
